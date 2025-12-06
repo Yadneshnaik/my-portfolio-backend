c3de39e5158184fafa3228fa2c10d73d7e21f02d
Yadnesh Naik <naikyadnesh9@gmail.com>
2025-12-06 09:20:50 +0530

    init

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/send-mail", async (req, res) => {
+app.post("/api/send-mail", async (req, res) => {
   const { name, email, message } = req.body;
 
   try {
